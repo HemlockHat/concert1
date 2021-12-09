37553e363e4d73ec946ca39f03ea1862c69f0395
gallegos.archer <galleos.archer@vashonsd.org>
2021-12-09 10:46:42 -0800

    Added two performers

--- src/org/vashonsd/Main.java
+++ src/org/vashonsd/Main.java
@@ -10,6 +10,18 @@ public class Main {
      * Then print out the performers.
      */
     public static void main(String[] args) {
+        System.out.println("Welcome to Desert Rocks, the hot new venue! Featuring: ");
+
+        Performer alan = new Performer("Alan", "has been performing for 6 years and lives in Ohio");
+
+        alan.addHit("food");
+        alan.addHit("water");
+        alan.addHit("cat food");
+        System.out.println(alan);
+
+        Performer jann = new Performer( "Jann", "Has been performing for 3 years and lives in Texas");
+        System.out.println(jann);
+        jann.addHit("see the sea");
 
     }
 }
